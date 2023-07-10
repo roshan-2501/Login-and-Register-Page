d4f1a6f71847dd0b41c06b80142647d383fb2659
roshan-2501 <117922504+roshan-2501@users.noreply.github.com>
2023-07-10 15:42:35 +0530

    Add files via upload

---
+++ Login and register/index.js
@@ -0,0 +1,22 @@
+const wrapper=document.querySelector('.wrapper');
+const loginLink=document.querySelector('.login-link');
+const registerLink=document.querySelector('.register-link');
+const btnLogin=document.querySelector('.btn-login');
+const closeicon=document.querySelector('.icon-close');
+
+registerLink.addEventListener('click',()=>{
+wrapper.classList.add('active');
+});
+
+
+loginLink.addEventListener('click',()=>{
+    wrapper.classList.remove('active');
+});
+
+btnLogin.addEventListener('click',()=>{
+    wrapper.classList.add('active-popup');
+});
+
+closeicon.addEventListener('click',()=>{
+    wrapper.classList.remove('active-popup');
+});
